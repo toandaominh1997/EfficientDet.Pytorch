20c7c401e319403d59f0fe9cf77dfe41646025c4
Đào Minh Toàn <v.toandm2@vinid.net>
2019-12-17 09:31:45 +0700

    Remove cuda in test.py

--- test.py
+++ test.py
@@ -2,10 +2,10 @@ import torch
 from models import EfficientDet
 
 if __name__ == '__main__':
-    inputs = torch.randn(5, 3, 512, 512).cuda()
+    inputs = torch.randn(5, 3, 512, 512)
 
     model = EfficientDet(num_classes=2, is_training=True)
-    model = model.cuda()
+    model = model
     output = model(inputs)
     for p in output:
         print(p.size())
