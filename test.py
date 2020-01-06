eab713bb6ccff3cd3343affd77c8897462fe830c
Đào Minh Toàn <v.toandm2@vinid.net>
2020-01-06 11:47:29 +0700

    Update test circle ci

--- test.py
+++ test.py
@@ -1,11 +1,24 @@
 import torch 
 from models import EfficientDet
+from models.efficientnet import EfficientNet
 
 if __name__ == '__main__':
-    inputs = torch.randn(5, 3, 512, 512)
 
-    model = EfficientDet(num_classes=2, is_training=True)
-    model = model
+    inputs = torch.randn(5, 3, 512, 512)
+    
+    # Test EfficientNet
+    model = EfficientNet.from_pretrained('efficientnet-b0')
+    inputs = torch.randn(4, 3, 512, 512)
+    P = model(inputs)
+    for idx, p in enumerate(P):
+        print('P{}: {}'.format(idx, p.size()))
+
+    # print('model: ', model) 
+
+
+
+    # Test inference
+    model = EfficientDet(num_classes=20, is_training=False)
     output = model(inputs)
-    for p in output:
-        print(p.size())
+    for out in output:
+        print(out.size())
