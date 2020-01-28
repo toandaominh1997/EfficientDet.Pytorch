aa100a7669c32bf7bff66fc3fd56125093b0be1f
Cyril Equilbec <39593288+Cyril9227@users.noreply.github.com>
2020-01-28 14:04:25 +0800

    Update efficientnet.py
    Removed duplicate function from_pretrained and added advprop option to from_pretrained function

--- models/efficientnet.py
+++ models/efficientnet.py
@@ -222,26 +222,17 @@ class EfficientNet(nn.Module):
         return cls(blocks_args, global_params)
 
     @classmethod
-    def from_pretrained(cls, model_name, num_classes=1000, in_channels=3):
+    def from_pretrained(cls, model_name, num_classes=1000, in_channels=3, advprop=False):
         model = cls.from_name(model_name, override_params={
                               'num_classes': num_classes})
         load_pretrained_weights(
-            model, model_name, load_fc=(num_classes == 1000))
+            model, model_name, load_fc=(num_classes == 1000), advprop=advprop)
         if in_channels != 3:
             Conv2d = get_same_padding_conv2d(
                 image_size=model._global_params.image_size)
             out_channels = round_filters(32, model._global_params)
             model._conv_stem = Conv2d(
                 in_channels, out_channels, kernel_size=3, stride=2, bias=False)
-        return model
-
-    @classmethod
-    def from_pretrained(cls, model_name, num_classes=1000):
-        model = cls.from_name(model_name, override_params={
-                              'num_classes': num_classes})
-        load_pretrained_weights(
-            model, model_name, load_fc=(num_classes == 1000))
-
         return model
 
     @classmethod
