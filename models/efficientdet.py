abbc5b886db2ee3aa3341eeb4593ced83e28f980
longyu <lolongcovas@gmail.com>
2020-04-03 17:41:35 +0200

    remove effcientnet from scratch

--- models/efficientdet.py
+++ models/efficientdet.py
@@ -47,15 +47,15 @@ class EfficientDet(nn.Module):
 
         # ============== original code starts ===============
         """The following code forces all weights to be random, which does not make sense at all!"""
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                m.weight.data.normal_(0, math.sqrt(2. / n))
-            elif isinstance(m, nn.BatchNorm2d):
-                m.weight.data.fill_(1)
-                m.bias.data.zero_()
+        # for m in self.modules():
+        #     if isinstance(m, nn.Conv2d):
+        #         n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+        #         m.weight.data.normal_(0, math.sqrt(2. / n))
+        #     elif isinstance(m, nn.BatchNorm2d):
+        #         m.weight.data.fill_(1)
+        #         m.bias.data.zero_()
 
-        self.freeze_bn()
+        # self.freeze_bn()
         # ============== original code ends ===============
 
         self.criterion = FocalLoss()
